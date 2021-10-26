0cad40db9ef53d34c6363fa78b05b4e330e8a057
milka77 <55999046+milka77@users.noreply.github.com>
2021-10-26 17:07:14 +0100

    Moved the list logic to a separate file ExpensesList.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 import "./Expenses.css";
+
 
 
 function Expenses(props) {
@@ -18,25 +19,12 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expenseContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem 
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onSelectYear={selectYearHandler}/>
 
-        {expenseContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
