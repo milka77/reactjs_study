b8c7365a28e1db96c12f35b6b1f68a81a70cf982
milka77 <55999046+milka77@users.noreply.github.com>
2021-10-29 19:30:56 +0100

    Added the chart element and chart bars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -23,6 +24,8 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onSelectYear={selectYearHandler}/>
+
+        <ExpensesChart expenses={filteredExpenses} />
 
         <ExpensesList items={filteredExpenses} />
       </Card>
